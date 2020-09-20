acc64172e614ec4112886d0aeecbcc85b3a5eaf0
NITHISH K MEGARAJAN <68291344+NithishK5@users.noreply.github.com>
2020-09-20 18:08:02 +0100

    connecting MongoDB

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect to database
+
+connectDB();
 
 app.use(express.json({ extended: false}));
 
